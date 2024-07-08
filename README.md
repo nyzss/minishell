aaaf80cfd7ec230a570a52631ef13b184858dd0a
Okan Emre KOCA <oknakoca@gmail.com>
2024-07-08 13:22:11 +0200

    feat: added data structures

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 hello world
 Hi there
+
+
+// void	get_redir(t_filenames *filenames, int *in_fd, int *out_fd)
+// {
+// 	while (filenames)
+// 	{
+// 		if filename->type == OUTFILE
+// 		{
+// 			*in_fd = open(filenames->filename, ...);
+// 		}
+// 		else if filename->type == INFILE
+// 		{
+// 			*in_fd = open(filenames->filename, ...);
+// 		}
+// 		if filename->type == APPEND
+// 		{
+// 			*in_fd = open(filenames->filename, ...);
+// 		}
+// 		if filename->type == HEREDOC
+// 		{
+// 			*in_fd = open(filenames->filename, ...);
+// 		}
+// 	}
+// }
--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 11:23:56 by okoca             #+#    #+#             */
-/*   Updated: 2024/07/08 11:45:56 by okoca            ###   ########.fr       */
+/*   Updated: 2024/07/08 13:16:05 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,59 @@
 # include <sys/wait.h>
 # include <signal.h>
 # include <fcntl.h>
+
+// ----------------------- LEXER -----------------------
+
+typedef enum e_token_type
+{
+	INFILE,
+	OUTFILE,
+	HEREDOC,
+	APPEND,
+	PIPE,
+	STRING,
+	COMMAND,
+	FILENAME,
+	ARGUMENT
+}	t_token_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
+
+// ----------------------- EXEC -----------------------
+
+typedef struct s_args
+{
+	char			*value;
+	struct s_args	*next;
+}	t_args;
+
+typedef struct s_filenames
+{
+	char				*path;
+	t_token_type		type;
+	struct s_filenames	*next;
+}	t_filenames;
+
+typedef struct s_exec
+{
+	char			*cmd;
+	t_args			*args;
+	t_filenames		*redirs;
+	struct s_exec	*next;
+}	t_exec;
+
+typedef struct s_ctx
+{
+	int			def_in;
+	int			def_out;
+	t_exec		*exec;
+	char		**env;
+}	t_ctx;
 
 void	builtins(void);
 void	exec(void);
