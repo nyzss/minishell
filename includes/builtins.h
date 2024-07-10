0e6ef7e952816b9c7bb714faa80f327edea43a1e
Tsunghao <tsunghao.chen12@gmail.com>
2024-07-10 16:14:36 +0200

    feat: re-arrange exec according to the need of built-ins. Started built-in part

--- includes/builtins.h
+++ includes/builtins.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 14:02:35 by okoca             #+#    #+#             */
-/*   Updated: 2024/07/08 14:03:23 by okoca            ###   ########.fr       */
+/*   Updated: 2024/07/10 16:12:08 by tsuchen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define BUILTINS_H
 
 void	builtins(void);
+int     bi_do_exec(char *cmd, t_args *args, char **env);
+int     bi_is_builtin(char *cmd);
 
 #endif
