1c33080b9fdba5b1a1db6615a1e8dd2d51635e36
Okan Emre KOCA <oknakoca@gmail.com>
2024-07-18 11:53:18 +0200

    fix: exit code and error message on is a directory/permission denied fixed.

--- src/exec/exec_err_utils.c
+++ src/exec/exec_err_utils.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 11:26:50 by tsuchen           #+#    #+#             */
-/*   Updated: 2024/07/15 22:55:04 by okoca            ###   ########.fr       */
+/*   Updated: 2024/07/18 11:47:53 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,12 +46,18 @@ void	exe_err3_fork(int err_no)
 
 void	exe_err4_exec(char *path, int err_no)
 {
-	int		fd_tmp;
+	int				fd_tmp;
+	struct stat		stats;
 
 	fd_tmp = dup(STDOUT_FILENO);
 	dup2(STDERR_FILENO, STDOUT_FILENO);
-	if (exe_is_abs_path(path) == 1)
-		printf("%s: %s: %s\n", P_NAME, path, strerror(err_no));
+	if (err_no == 13 && stat(path, &stats) != -1)
+	{
+		if (S_ISDIR(stats.st_mode) == 1)
+			printf("%s: %s: Is a directory\n", P_NAME, path);
+		else
+			printf("%s: %s: %s\n", P_NAME, path, strerror(err_no));
+	}
 	else
 		printf("%s: %s: command not found\n", P_NAME, path);
 	exe_dup2_close(fd_tmp, STDOUT_FILENO);
